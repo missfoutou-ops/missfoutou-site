87d8beb7ae25824907fe7515b68812494b274b9b
missfoutou-ops <missfoutou@gmail.com>
2025-11-02 23:54:07 +0100

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,1 @@
+document.addEventListener('click',e=>{const a=e.target.closest('a[href^="#"]');if(a){e.preventDefault();const id=a.getAttribute('href').slice(1);const el=document.getElementById(id);if(el)el.scrollIntoView({behavior:'smooth'})}});
